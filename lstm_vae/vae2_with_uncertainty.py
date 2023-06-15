f84beb9e26bb537107525b73f0b735ffd0cf3298
jamesonquinn <jameson.quinn+github@gmail.com>
2023-06-15 09:17:15 -0400

    code for v1.0

--- lstm_vae/vae2_with_uncertainty.py
+++ lstm_vae/vae2_with_uncertainty.py
@@ -1,17 +1,16 @@
 import numpy as np
 import tensorflow as tf
 
-
 import keras
 from keras import backend as K
 from keras.models import Model
 from keras.layers import Input, LSTM, RepeatVector, Concatenate, Layer
 from keras.layers.core import Dense, Lambda
+from keras.optimizers import Adam
 from keras import metrics as objectives
-from keras.optimizers import Adam, Adadelta, RMSprop
-from keras.callbacks import Callback
-import tensorflow_addons as tfa
-from tensorflow_addons.optimizers import MultiOptimizer
+from keras.regularizers import L1
+from tensorflow.keras.constraints import MaxNorm
+
 
 def page_row_to_vec(row):
     return [np.log(row.len) / 4,np.sqrt(row.left),row.offset,row.height,(row.left + row.width) ** 2,row.confidence / 100,row.near_bottom]
@@ -34,13 +33,8 @@ def pad_page_to(page, target=40):
     return first_half + second_half
 
 PARTIALLY_UNMASK_FUDGE = 0.01
-MIN_Z_SIGMA = 0.05
+MIN_Z_SIGMA = 0.005
 X_SIGMA_TRANSFORM_FACTOR = 0.49
-EON_EPOCHS = 30
-KL_START_EPOCH = 5
-KL_ANNEAL_TIME = 9 #so 10 epochs of KL weight=min, 18 to anneal, ~30 epochs of weight=1, then repeat.
-MIN_KL_WEIGHT = 0.1
-BASE_LR = 0.002
 
 def create_lstm_uvae(input_dim, 
     timesteps, 
@@ -79,18 +73,16 @@ def create_lstm_uvae(input_dim,
     h = LSTM(intermediate_dim)(x)
 
     # VAE Z layer
-    z_mean = Dense(latent_dim, name="z_mean")(h)
-    z_sigma_factor = Dense(latent_dim, name="z_sigma_factor")(h)
+    z_mean = Dense(latent_dim, kernel_regularizer=L1(0.0002))(h)
+    z_sigma_factor = Dense(latent_dim, kernel_regularizer=L1(0.0002))(h)
     # [-1, 1] not [0, 1] but the way we use it, the sign is irrelevant
     
-    weight = K.variable(0.0)
-
     def sampling(args):
         z_mean, z_sigma_factor = args
         epsilon = K.random_normal(shape=(batch_size, latent_dim),
                                   mean=0., stddev=epsilon_std)
         #print("sampling", z_mean.shape, z_sigma_factor.shape, epsilon.shape)
-        return z_mean + (K.abs(z_sigma_factor) + MIN_Z_SIGMA) * epsilon #* weight  #don't temper by weight, because we're going to temper the lr instead
+        return z_mean + (K.abs(z_sigma_factor) + MIN_Z_SIGMA) * epsilon
 
     # note that "output_shape" isn't necessary with the TensorFlow backend
     # so you could write `Lambda(sampling)([z_mean, z_sigma_intervaltransform])`
@@ -124,18 +116,6 @@ def create_lstm_uvae(input_dim,
     # end-to-end autoencoder
     vae = Model(x, [x_decoded_mean, x_decoded_sigma_intervaltransform, combined_x_decoded])
 
-    for i, layer in enumerate(vae.layers):
-        if layer.name == "z_sigma_factor":
-            z_sig_layer_index = i
-            break
-
-
-    steady_optimizer = RMSprop(momentum = 0.05, learning_rate=BASE_LR)
-    jerky_optimizer = RMSprop(learning_rate=BASE_LR)
-    optimizers_and_layers = [(jerky_optimizer, vae.layers[z_sig_layer_index]), 
-                             (steady_optimizer, vae.layers[:i] + vae.layers[i+1:])]
-    optimizr = MultiOptimizer(optimizers_and_layers)
-
     # encoder, from inputs to latent space
     encoder = Model(x, z_mean)
 
@@ -148,39 +128,23 @@ def create_lstm_uvae(input_dim,
     _x_decoded_mean = decoder_mean(_h_decoded)
     generator = Model(decoder_input, _x_decoded_mean)
     
-    class AnnealingCallback(Callback):
-        def __init__(self, weight):
-            self.weight = weight
-        def on_epoch_end (self, epoch, logs={}):
-            effective_epoch = epoch % EON_EPOCHS
-            if effective_epoch == 0:
-                K.set_value(self.weight, MIN_KL_WEIGHT)
-            if (effective_epoch) >= KL_START_EPOCH :
-                new_weight = min(K.get_value(self.weight) + ((1. - MIN_KL_WEIGHT)/ KL_ANNEAL_TIME), 1.)
-                K.set_value(self.weight, new_weight)
-            K.set_value(jerky_optimizer.learning_rate, BASE_LR * K.get_value(self.weight) * 1.4) #should average out to about BASE_LR
-            print (" KL fac = " + str(K.get_value(self.weight)))
+    def combined_loss(x_possibly_backwards, outputs):
+        x_decoded_mean, x_decoded_sigma_intervaltransform = outputs[:, :, :input_dim], outputs[:, :, input_dim:]
+        #print(x_possibly_backwards.shape, x_decoded_mean.shape, x_decoded_sigma_intervaltransform.shape)
+        x_decoded_sigma_intervaltransform = x_decoded_sigma_intervaltransform * X_SIGMA_TRANSFORM_FACTOR + .5 # map [-1, 1] to [0, 1]
+        x_decoded_sigma = (x_decoded_sigma_intervaltransform/(1-x_decoded_sigma_intervaltransform)) / 2 # map [0, 1] to [0, inf]
+        mostly_mask =  (tf.sign(x_possibly_backwards) ** 2 + .01)
+        #return K.sum(K.abs(outputs))
+        xent_loss = (K.mean(K.square((x_possibly_backwards - x_decoded_mean) / x_decoded_sigma) * mostly_mask) * .5 + #only count loss on the "real" part of the input
+                    K.mean(K.log(x_decoded_sigma) * mostly_mask) ) #add in the loss from the "uncertainty" in the reconstruction
+        z_sigma_corrected = K.abs(z_sigma_factor) + MIN_Z_SIGMA
+        kl_loss = - 0.5 * K.mean(1 - z_sigma_corrected - K.square(z_mean) + K.log(z_sigma_corrected))
+        loss = xent_loss + kl_loss
+        return loss
 
-
+    adam_optimizer = Adam(
+        learning_rate=0.002) #5 times the default, because we're using dropout
+    vae.compile(optimizer=adam_optimizer, loss={"combined_decoder": combined_loss})
     
-    def vae_loss(weight):
-        def combined_loss(x_possibly_backwards, outputs):
-            x_decoded_mean, x_decoded_sigma_intervaltransform = outputs[:, :, :input_dim], outputs[:, :, input_dim:]
-            #print(x_possibly_backwards.shape, x_decoded_mean.shape, x_decoded_sigma_intervaltransform.shape)
-            x_decoded_sigma_intervaltransform = x_decoded_sigma_intervaltransform * X_SIGMA_TRANSFORM_FACTOR + .5 # map [-1, 1] to [0, 1]
-            x_decoded_sigma = (x_decoded_sigma_intervaltransform/(1-x_decoded_sigma_intervaltransform)) / 2 # map [0, 1] to [0, inf]
-            mostly_mask =  (tf.sign(x_possibly_backwards) ** 2 + .01)
-            #return K.sum(K.abs(outputs))
-            xent_loss = (K.mean(K.square((x_possibly_backwards - x_decoded_mean) / x_decoded_sigma) * mostly_mask) * .5 + #only count loss on the "real" part of the input
-                        K.mean(K.log(x_decoded_sigma) * mostly_mask) ) #add in the loss from the "uncertainty" in the reconstruction
-            z_sigma_corrected = K.abs(z_sigma_factor) + MIN_Z_SIGMA
-            kl_loss = - 0.5 * K.mean(1 - z_sigma_corrected - K.square(z_mean) + K.log(z_sigma_corrected))
-            loss = xent_loss + weight * kl_loss
-            return loss
-        return combined_loss
-
-    optimizr = RMSprop(momentum = 0.05)#learning_rate=1.0)
-    vae.compile(optimizer=optimizr, loss={"combined_decoder": vae_loss(weight)})
-    
-    return vae, encoder, generator, AnnealingCallback(weight)
+    return vae, encoder, generator
 
